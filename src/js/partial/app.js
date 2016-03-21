0361478d9a4788c7222b13b7529ecac45408b00b
karachevtsev <karachevtsev@yahoo.com>
2016-03-21 19:05:11 +0200

    Add script for toggle current link

--- src/js/partial/app.js
+++ src/js/partial/app.js
@@ -1,3 +1,11 @@
 // write your code here
-
+$(function () { 
+    $('.menu__link').each(function () {
+        var location = window.location.href; 
+        var link = this.href;
+        if(location == link) {
+            $(this).addClass('menu__link_current'); 
+        }
+    });
+});
 
